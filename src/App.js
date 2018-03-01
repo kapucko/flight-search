67234af7704ff77fe5ddbef951428a46b7b30bec
Dominik Kapisinsky <dkapisinsky@cnc.sk>
2018-03-02 00:35:45 +0100

    renamed module

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SearchApp from './SearchFrom';
+import SearchApp from './Search';
 
 
 class App extends Component {
